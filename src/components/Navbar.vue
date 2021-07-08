a8983774259174b2a58f9a8557815cd088b2deac
Ivan Neverovsky <ivan@neverovsky.com.ua>
2021-07-08 12:55:47 +0300

    navbar and logout

---
+++ src/components/Navbar.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <img src="@/assets/ninja.png" alt="logo" />
+      <h1><router-link :to="{ name: 'Home' }">Muso Ninjas</router-link></h1>
+      <div class="links">
+        <button @click="handleLogout" v-if="user">Logout</button>
+        <template v-else>
+          <router-link class="btn" :to="{ name: 'Signup' }"
+            >Sign up</router-link
+          >
+          <router-link class="btn" :to="{ name: 'Login' }">Log in</router-link>
+        </template>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import { useRouter } from "vue-router";
+import useLogout from "@/composables/useLogout";
+import getUser from "@/composables/getUser";
+
+export default {
+  setup() {
+    const { logout } = useLogout();
+    const { user } = getUser();
+
+    const router = useRouter();
+
+    const handleLogout = async () => {
+      await logout();
+      console.log("user logged out");
+      router.push({ name: "Login" });
+    };
+
+    return { handleLogout, user };
+  },
+};
+</script>
+
+<style>
+.navbar {
+  padding: 16px 10px;
+  margin-bottom: 60px;
+  background: #fff;
+}
+nav {
+  display: flex;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+nav h1 {
+  margin-left: 20px;
+}
+nav .links {
+  margin-left: auto;
+}
+nav .links a,
+button {
+  margin-left: 16px;
+  font-size: 14px;
+}
+nav img {
+  max-height: 60px;
+}
+</style>
